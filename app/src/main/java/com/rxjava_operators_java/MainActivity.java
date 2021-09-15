cc8a1d23b6b491ec48b9553f8fdf1605e8cd3e4f
sateesh <sateesh147852@gmail.com>
2021-09-15 17:24:57 +0530

    create observable added

--- app/src/main/java/com/rxjava_operators_java/MainActivity.java
+++ app/src/main/java/com/rxjava_operators_java/MainActivity.java
@@ -1,14 +1,27 @@
 package com.rxjava_operators_java;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.rxjava_operators_java.databinding.ActivityMainBinding;
+import com.rxjava_operators_java.model.User;
+import com.rxjava_operators_java.utility.Utilites;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private String TAG = "MainActivity";
+    private Disposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +30,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.btSubscribe.setOnClickListener(v -> {
-
+            createObservable();
         });
 
+    }
+
+    private void createObservable() {
+
+        Observable observable = Observable.create(new ObservableOnSubscribe() {
+            @Override
+            public void subscribe(ObservableEmitter emitter) throws Exception {
+
+                if (!emitter.isDisposed()){
+                    for (User user : Utilites.getUsers())
+                        emitter.onNext(user);
+                    emitter.onComplete();
+                }
+
+            }
+        });
+
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer() {
+                    @Override
+                    public void onSubscribe( Disposable d) {
+                        disposable = d;
+                        Log.i(TAG, "onSubscribe: ");
+                    }
+
+                    @Override
+                    public void onNext( Object o) {
+                        Log.i(TAG, "onNext: "+o.toString());
+                    }
+
+                    @Override
+                    public void onError( Throwable e) {
+                        Log.i(TAG, "onError: "+e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.i(TAG, "onComplete: ");
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposable.dispose();
     }
 }
