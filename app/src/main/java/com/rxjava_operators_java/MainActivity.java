0f3ab4e14811b0fb8ea9bd23dc251eb1634f2301
sateesh <sateesh147852@gmail.com>
2021-09-15 16:57:43 +0530

    feature: user class added

--- app/src/main/java/com/rxjava_operators_java/MainActivity.java
+++ app/src/main/java/com/rxjava_operators_java/MainActivity.java
@@ -1,14 +1,24 @@
 package com.rxjava_operators_java;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.rxjava_operators_java.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btSubscribe.setOnClickListener(v -> {
+
+        });
+
     }
 }
---
+++ app/src/main/java/com/rxjava_operators_java/utility/Utilites.java
@@ -0,0 +1,46 @@
+package com.rxjava_operators_java.utility;
+
+import com.rxjava_operators_java.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Utilites {
+
+
+    public static String[] getStringArray(){
+        return new String[]{"Apple","Mango","Orange","Banana","Grapes"};
+    }
+
+    public static Integer[] getIntegerArray(){
+        return new Integer[]{1,2,3,4,5,6};
+    }
+
+
+    public static List<User> getUsers(){
+        List<User> users = new ArrayList<>();
+
+        users.add(new User("1","Sateesh"));
+        users.add(new User("2","Anand"));
+        users.add(new User("3","Kiran"));
+        users.add(new User("4","Suresh"));
+        users.add(new User("5","Vikram"));
+        return users;
+    }
+
+
+    public static List<String> getStringData(){
+        List<String> data = new ArrayList<>();
+
+        data.add("ABC");
+        data.add("DEF");
+        data.add("PQR");
+        data.add("LMN");
+        data.add("XYZ");
+
+        return data;
+    }
+
+
+
+}
