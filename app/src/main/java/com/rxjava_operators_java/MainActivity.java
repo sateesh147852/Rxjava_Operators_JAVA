731571a45ecc50a58750168c06e2958325f7f241
sateesh <sateesh147852@gmail.com>
2021-09-17 10:58:06 +0530

    initial commit

--- app/src/main/java/com/rxjava_operators_java/MainActivity.java
+++ app/src/main/java/com/rxjava_operators_java/MainActivity.java
@@ -1,13 +1,20 @@
 package com.rxjava_operators_java;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.rxjava_operators_java.databinding.ActivityMainBinding;
+import com.rxjava_operators_java.model.User;
+import com.rxjava_operators_java.utility.Utilites;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,13 +30,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.btSubscribe.setOnClickListener(v -> {
-
+            createObservable();
         });
+
+    }
+
+    private void createObservable() {
+
+        Observable.fromIterable(Utilites.getUsers())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<User>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable = d;
+                        Log.i(TAG, "onSubscribe: ");
+                    }
+
+                    @Override
+                    public void onNext(User user) {
+                        Log.i(TAG, "onNext: "+user.toString());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.i(TAG, "onError: "+e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.i(TAG, "onComplete: ");
+                    }
+                });
 
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        disposable.dispose();
     }
 }
