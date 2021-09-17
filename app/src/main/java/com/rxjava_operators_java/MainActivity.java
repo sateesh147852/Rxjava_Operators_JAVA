2d6743754ed699a1b3016d7216bf9536a4d78dd7
sateesh <sateesh147852@gmail.com>
2021-09-17 22:50:48 +0530

    initial commit

--- app/src/main/java/com/rxjava_operators_java/MainActivity.java
+++ app/src/main/java/com/rxjava_operators_java/MainActivity.java
@@ -1,13 +1,18 @@
 package com.rxjava_operators_java;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.rxjava_operators_java.databinding.ActivityMainBinding;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,8 +28,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.btSubscribe.setOnClickListener(v -> {
-
+            //createTakeObservable();
+            createTakeLastObservable();
         });
+
+    }
+
+    private void createTakeLastObservable() {
+
+        Observable.range(1,10)
+                .subscribeOn(Schedulers.io())
+                .takeLast(5)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable = d;
+                        Log.i(TAG, "onSubscribe: ");
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.i(TAG, "onNext: "+integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.i(TAG, "onError: "+e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.i(TAG, "onComplete: ");
+                    }
+                });
+
+    }
+
+    private void createTakeObservable() {
+
+        Observable.range(1,10)
+                .subscribeOn(Schedulers.io())
+                .take(5)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable = d;
+                        Log.i(TAG, "onSubscribe: ");
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.i(TAG, "onNext: "+integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.i(TAG, "onError: "+e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.i(TAG, "onComplete: ");
+                    }
+                });
 
     }
 
